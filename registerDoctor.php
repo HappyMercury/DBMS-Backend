c3f40df6823dedbb914f969dd4f6b2f6cbdcabae
Harsh Gupta <53963325+HappyMercury@users.noreply.github.com>
2021-04-03 03:06:44 +0530

    Update registerDoctor.php
    Added the changes for department and hospital entry

--- registerDoctor.php
+++ registerDoctor.php
@@ -18,8 +18,47 @@
 	$hospital = $_POST['hospital'];
 	$department = $_POST['department'];
 
-	//first registering hospital
-	// $query = "INSERT INTO hospital VALUES "
+	//for department
+	$dnumQuery = "SELECT dnum FROM department WHERE dname=?";
+
+	$deptStmt = $conn->prepare($dnumQuery);
+	$deptStmt->bind_param("s",$department);
+	$deptStmt->execute();
+	$deptStmt->store_result();
+
+	if($deptStmt->num_rows>0){
+		$deptStmt->bind_result($dno);
+		$deptStmt->fetch();
+		$dnum = $dno;
+	}
+	else{
+		$dnumQuery = "INSERT INTO department (dname) VALUES(?);";
+
+		$deptStmt = $conn->prepare($dnumQuery);
+		$deptStmt->bind_param("s",$department);
+		$deptStmt->execute();
+
+		$dnumQuery = "SELECT dnum FROM department WHERE dname=?";
+
+		$deptStmt = $conn->prepare($dnumQuery);
+		$deptStmt->bind_param("s",$department);
+		$deptStmt->execute();
+		$deptStmt->store_result();
+		$deptStmt->bind_result($dno);
+		$deptStmt->fetch();
+		$dnum = $dno;
+	}
+
+	//for hospital
+	$hnumQuery = "SELECT hnum FROM hospital WHERE hname=?";
+
+	$deptStmt = $conn->prepare($dnumQuery);
+	$deptStmt->bind_param("s",$hospital);
+	$deptStmt->execute();
+	$deptStmt->store_result();
+	$deptStmt->bind_result($hno);
+	$deptStmt->fetch();
+	$hnum = $hno;
 
 	//making prepared query
 	$stmt = $conn->prepare("INSERT INTO doctor (name,phone,age,address,email,password,auth,dnum,hnum) VALUES(?,?,?,?,?,?,?,?,?)");
