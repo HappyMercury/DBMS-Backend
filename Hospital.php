0a2bba0d203ea84d3ed6c97844da3e716f9c84cb
Harsh Gupta <53963325+HappyMercury@users.noreply.github.com>
2021-04-02 11:31:41 +0530

    Updated hospital

--- Hospital.php
+++ Hospital.php
@@ -16,3 +16,32 @@ class Hospital
 
 	}
 }
+
+$hospital = $_POST['hospital'];
+$street = null;
+$area = null;
+$landmark = null;
+
+$query = "SELECT * FROM hospital WHERE hname=?";
+
+$stmt =  $conn->prepare($query);
+$stmt->bind_param("s",$hospital);
+$stmt->execute();
+
+$stmt->store_result();
+
+if($stmt->num_rows>0)
+{
+	//hospital already exists
+	//can insert value of doctor without asking for address
+	
+	
+}
+else
+{
+	//ask for location which includes street,area,landmark
+	$myObj->ask = "yes";
+	$json = json_encode($myObj);
+	echo $json;
+}
+?>
