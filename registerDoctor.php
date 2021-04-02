530b315f8c1a709e1d9d0f37145f12071152c9b8
Harsh Gupta <53963325+HappyMercury@users.noreply.github.com>
2021-04-03 04:08:41 +0530

    Update registerDoctor.php
    corrected the parameters for doctor

--- registerDoctor.php
+++ registerDoctor.php
@@ -9,8 +9,7 @@
 	//variables to get from application using POST
 	$name = $_POST["name"];
 	$phone = $_POST["phone"];
-	$age = $_POST["age"];
-	$address = $_POST["address"];
+	$specialization = $_POST["age"];
 	$email = $_POST["email"];
 	$password = $_POST["password"];
 	$password = md5($password);
@@ -19,12 +18,13 @@
 	$department = $_POST['department'];
 
 	//for department
-	$dnumQuery = "SELECT dnum FROM department WHERE dname=?";
+	$dnumQuery = "SELECT dep_id FROM department WHERE dep_name=?";
 
 	$deptStmt = $conn->prepare($dnumQuery);
 	$deptStmt->bind_param("s",$department);
 	$deptStmt->execute();
 	$deptStmt->store_result();
+
 
 	if($deptStmt->num_rows>0){
 		$deptStmt->bind_result($dno);
@@ -32,13 +32,13 @@
 		$dnum = $dno;
 	}
 	else{
-		$dnumQuery = "INSERT INTO department (dname) VALUES(?);";
+		$dnumQuery = "INSERT INTO department (dep_name) VALUES(?);";
 
 		$deptStmt = $conn->prepare($dnumQuery);
 		$deptStmt->bind_param("s",$department);
 		$deptStmt->execute();
 
-		$dnumQuery = "SELECT dnum FROM department WHERE dname=?";
+		$dnumQuery = "SELECT dep_id FROM department WHERE dep_name=?";
 
 		$deptStmt = $conn->prepare($dnumQuery);
 		$deptStmt->bind_param("s",$department);
@@ -50,9 +50,9 @@
 	}
 
 	//for hospital
-	$hnumQuery = "SELECT hnum FROM hospital WHERE hname=?";
+	$hnumQuery = "SELECT hid FROM hospital WHERE hname=?";
 
-	$deptStmt = $conn->prepare($dnumQuery);
+	$deptStmt = $conn->prepare($hnumQuery);
 	$deptStmt->bind_param("s",$hospital);
 	$deptStmt->execute();
 	$deptStmt->store_result();
@@ -61,14 +61,27 @@
 	$hnum = $hno;
 
 	//making prepared query
-	$stmt = $conn->prepare("INSERT INTO doctor (name,phone,age,address,email,password,auth,dnum,hnum) VALUES(?,?,?,?,?,?,?,?,?)");
+	$stmt = $conn->prepare("INSERT INTO doctor (name,specialization,phone,email,password,auth,dnum,hnum) VALUES(?,?,?,?,?,?,?,?)");
 
 	//binding parameters for placeholder markers
-	$stmt->bind_param("ssisssiii",$name,$phone,$age,$address,$email,$password,$auth,$dnum,$hnum);
+	$stmt->bind_param("sssssiii",$name,$specialization,$phone,$email,$password,$auth,$dnum,$hnum);
 
 	//executing query
 	$stmt->execute();
-
+	
+	//sending back details after registration
+	$data = array('name' => $name,
+	                'specialization' => $specialization,
+	                'phone' => $phone,
+	                'email' => $email,
+	                'auth' => $auth,
+	                'hospital' => $hospital,
+	                'department' => $department);
+	                
+	 $json = json_encode($data);
+	 echo $json;
+	
+	$deptStmt->close();
 	$conn->close();
 	$stmt->close();
 
