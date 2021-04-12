579a1aa24a17b835527e04e4cf8d78af912f21e0
Harsh Gupta <53963325+HappyMercury@users.noreply.github.com>
2021-04-12 16:58:14 +0530

    Update Department.php
    corrected the response format

--- Department.php
+++ Department.php
@@ -7,38 +7,39 @@ $conn = $db->connect();
 $query = "SELECT dep_id,dep_name FROM department";
 
 $stmt = $conn->prepare($query);
-$stmt->bind_param();
 
 //json paramas
 $error = false;
 $message = "success";
 $result;
 
-$id = array();
-$name = array();
+// $id = array();
+// $name = array();
+$department = array();
 
 if($stmt->execute())
 {
     $stmt->store_result();
     $stmt->bind_result($dep_id,$dep_name);
     $i = 0;
+    
     while($stmt->fetch())
     {
-        $id[$i] = $dep_id;
-        $name[$i] = $dep_name;
-        $i++;
+        // $id[$i] = $dep_id;
+        // $name[$i] = $dep_name;
+        $department[$i++] = array("dep_id" => $dep_id,"dep_name" => $dep_name);
     }
     http_response_code(200);
     
-    $result = array(
-        "dep_id" => $id,
-        "dep_name" => $name);
+    $result = array();
+    $result["departments"] = $department;
+    
 }
 else
 {
     $error = true;
     $message = "failure";
-    $result = array();
+    $result = null;
     http_response_code(200);
 }
 
@@ -48,7 +49,7 @@ $data = array(
     "result" => $result
     );
 
-$json = json_encode($data,JSON_FORCE_OBJECT);
+$json = json_encode($data);
 
 echo $json;
 
