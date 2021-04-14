2f67f780b3919dfb305e5f2e4da64b6ad3f3582e
HappyMercury <harshgup01@gmail.com>
2021-04-14 15:31:59 +0530

    correct 500 error

--- registerHospital.php
+++ registerHospital.php
@@ -2,127 +2,69 @@
 
 include 'Database.php';
 
-
-
 $db = new Database();
-
 $conn = $db->connect();
 
-
-
 //variables for hospital
-
 $hname = $_POST['hname'];
-
 $street = $_POST['street'];
-
 $area = $_POST['area'];
 
-
-
 //response params
-
 $error = false;
-
 $message = "success";
-
-$result = null;
-
-
+$result;
 
 $query = "INSERT INTO hospital (hname,street,area) VALUES (?,?,?)";
 
+$stmt = $conn->prepare($query);
+$stmt->bind_param("sss",$hname,$street,$area);
 
+if($stmt->execute()) {
 
-if($stmt =  $conn->prepare($query)) {
+	$hospQuery = "SELECT hid,hname,street,area FROM hospital WHERE hname=?";
+	$hospStmt = $conn->prepare($hospQuery);
+	$hospStmt->bind_param("s",$hname);
 
+	$hospStmt->execute();
 
-
-	$stmt->bind_param("sss",$hname,$street,$area);
-
-	$stmt->execute();
-
+	$hospStmt->store_result();
+	$hospStmt->bind_result($hid,$hname,$street,$area);
+    $hospStmt->fetch();
 	
-
-	$query = "SELECT hid,hname,street,area FROM hospital WHERE hname=?";
-
-	$stmt = $conn->prepare($query);
-
-	$stmt->bind_param("s",$hname);
-
-	$stmt->execute();
-
-	$stmt->store_result();
-
-	$stmt->bind_result($hid,$hname,$street,$area);
-
-    $stmt->fetch();
-	
-
-	$hospital = array(
-
+	$hospital[0] = array(
 	    "hid" => $hid,
-
 	    "hname" => $hname,
-
 	    "street" => $street,
-
 	    "area" => $area
-
 	    );
 
-    $result["hospital"] => $hospital;
-
+    $result["hospital"] = $hospital;
     $error = false;
-
     $message = "success";
-
     http_response_code(200);
 
-	
-
 }
-
 else
-
 {
-
     $error = true;
-
     $message = "failure";
-
     $result = null;
-
     http_response_code(200);
-
 }
-
-
 
 $data = array(
-
     "error" => $error,
-
     "message" => $message,
-
     "result" => $result
-
     );
-
-    
 
     $json = json_encode($data);
 
-    
-
     echo $json;
 
-    
-
     $stmt->close();
-
     $conn->close();
-
 
 
 ?>
