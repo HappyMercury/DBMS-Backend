3fea2d8988bc6032d3434dae5d5287ba94ff04e4
HappyMercury <harshgup01@gmail.com>
2021-04-16 12:28:11 +0530

    created

---
+++ createMedicalRecord.php
@@ -0,0 +1,60 @@
+<?php
+include 'Database.php';
+
+$db = new Database();
+$conn = $db->connect();
+
+$pid = $_POST['pid'];
+$date = $_POST['date'];
+$attachment = $_POST['attachment'];
+$diagnosis = $_POST['diagnosis'];
+$description = $_POST['description'];
+
+//converting time into day,month,year,hour,minute,second
+$tok = strtok($date, "-");
+$i = 0;
+while ($tok !== false) 
+{
+    $time[$i++] = $tok;
+    $tok = strtok("-");
+}
+
+$day =  $time[0];
+$month = $time[1];
+$year = $time[2];
+
+$timestamp = mktime(0,0,0,$month,$day,$year);
+
+
+$query = "INSERT INTO medical_records (pid,DOE,description,diagnosis,attachment) VALUES (?,?,?,?,?)";
+
+$stmt = $conn->prepare($query);
+$stmt->bind_param("iiss",$pid,$timestamp,$description,$diagnosis,$attachment);
+if($stmt->execute())
+{
+    $error = false;
+    $message = "success";
+    $result = (object) [];
+    http_response_code(200);
+}
+else{
+    $error = true;
+    $message = "failure";
+    $result = null;
+    http_response_code(200);
+}
+
+$data = array(
+    "error" => $error,
+    "message" => $message,
+    "result" => $result
+);
+
+$json = json_encode($data);
+
+echo $json;
+
+$stmt->close();
+$conn->close();
+
+?>
