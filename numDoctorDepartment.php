bfe6c3d5c40d59737ab786d9224aabc167033141
HappyMercury <harshgup01@gmail.com>
2021-04-19 13:37:44 +0530

    for departments.retruning number of doctors in the department

---
+++ numDoctorDepartment.php
@@ -0,0 +1,52 @@
+<?php
+include 'Database.php';
+
+$db = new Database();
+$conn = $db->connect();
+
+//json params;
+$error = false;
+$message = "success";
+$result;
+
+$query = "SELECT dep_id,dep_name,count(*) FROM department,doctor WHERE dep_id=dnum GROUP BY dep_id";
+
+$stmt = $conn->prepare($query);
+
+if($stmt->execute())
+{
+    $stmt->store_result();
+    $stmt->bind_result($dep_id,$dep_name,$count);
+    
+    $i = 0;
+    
+    $result = array();
+
+    while($stmt->fetch())
+    {
+        $doctorDepartment[$i++] = array("dep_id" => $dep_id,"dep_name" => $dep_name,"count" => $count);
+    }
+    $result['doctorDepartment'] = $doctorDepartment;
+    http_response_code(200);
+}
+else
+{
+    $error = true;
+    $message = "failure";
+    $result = array();
+    http_response_code(200);
+}
+
+$data = array(
+    "error" => $error,
+    "message" => $message,
+    "result" => $result
+    );
+
+$json = json_encode($data);
+echo $json;
+
+$stmt->close();
+$conn->close();
+
+?>
